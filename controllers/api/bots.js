c3bd4e6e52564f206341a2a201a125c1eaed5a90
Laurent Debacker <debackerlaurent@gmail.com>
2018-06-26 22:04:20 -0700

    Moved msg parsing to post

--- controllers/api/bots.js
+++ controllers/api/bots.js
@@ -166,7 +166,26 @@ var Game = function(id) {
 };
 
 router.post('/', function (req, res, next) {
-  console.log(req.body);
+  var msg = {
+    id: req.body.message.message_id,
+    from: req.body.message.from,
+    text: req.body.message.text,
+    chat: req.body.message.chat
+  };
+  console.log(msg);
+  if (msg.text === '/start') {
+    b.sendMessage(msg.chat.id, 'To start a game, type /newgame');
+  } else if (msg.text === '/newgame' && !games[msg.chat.id]) {
+    games[msg.chat.id] = new Game(msg.chat.id);
+    b.sendMessage(msg.chat.id, bot.list.name);
+  } else if (msg.text === '/newgame' && games[msg.chat.id]) {
+    b.sendMessage(msg.chat.id, 'A game is already in progress');
+  } else if (msg.text === '/stop' && games[msg.chat.id]) {
+    delete games[msg.chat.id];
+    b.sendMessage(msg.chat.id, 'Game stopped');
+  } else {
+
+  }
   b.sendMessage(500242425, 'Received');
   res.json({ message: 'post ok'});
 });
