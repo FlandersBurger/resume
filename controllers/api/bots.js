e45980dfaf693f976f6cbd7f38a55edc0e6c9311
Laurent Debacker <debackerlaurent@gmail.com>
2018-07-03 21:36:52 -0700

    Bot should now chek answers

--- controllers/api/bots.js
+++ controllers/api/bots.js
@@ -7,18 +7,18 @@ var games = {};
 var lists = [
   {
     name: 'Camera Brands',
-    values: {
-      'nikon': null,
-      'canon': null,
-      'sony': null,
-      'pentax': null,
-      'olympus': null,
-      'fujifilm': null,
-      'kodak': null,
-      'leica': null,
-      'panasonic': null,
-      'lumix': null
-    }
+    values: [
+      { value: 'nikon' },
+      { value: 'canon' },
+      { value: 'sony' },
+      { value: 'pentax' },
+      { value: 'olympus' },
+      { value: 'fujifilm' },
+      { value: 'kodak' },
+      { value: 'leica' },
+      { value: 'panasonic' },
+      { value: 'lumix' }
+    ]
   }
 ];
 
@@ -81,53 +81,6 @@ function Bot() {
     });
   };
 
-  bot.getUpdates = function() {
-    return new Promise(function (resolve, reject) {
-      var url = 'https://api.telegram.org/bot' + TOKEN + '/getUpdates';
-      request(url, function (error, r, body) {
-        var response = JSON.parse(body).result;
-        if(error) return;
-        if(!response) return;
-        var lastMsgId = 0;
-        var messages = response.map(function(msg) {
-          if (msg.message) {
-            if (lastMsgId < msg.message.message_id) lastMsgId = msg.message.message_id;
-            return {
-              id: msg.message.message_id,
-              from: msg.message.from,
-              text: msg.message.text,
-              chat: msg.message.chat
-            };
-          } else {
-            return {
-              id: null,
-            };
-          }
-        }).filter(function(msg) {
-          if (!msg.id) return false;
-          if (msg.chat.id !== 592503547) return false;
-          if (msg.from.id === 500242425) return false;
-          return msg.id > bot.lastMsgId;
-        }).forEach(function(msg) {
-          bot.lastMsgId = msg.id;
-          if (msg.text === '/start') {
-            bot.sendMessage(msg.chat.id, 'To start a game, type /newgame');
-          } else if (msg.text === '/newgame' && !games[msg.chat.id]) {
-            games[msg.chat.id] = new Game(msg.chat.id);
-            bot.sendMessage(msg.chat.id, bot.list.name);
-          } else if (msg.text === '/newgame' && games[msg.chat.id]) {
-            bot.sendMessage(msg.chat.id, 'A game is already in progress');
-          } else if (msg.text === '/stop' && games[msg.chat.id]) {
-            delete games[msg.chat.id];
-            bot.sendMessage(msg.chat.id, 'Game stopped');
-          } else {
-
-          }
-        });
-        resolve();
-      });
-    });
-  };
   bot.getName = function() {
     if (bot.last_name) {
       return bot.first_name + ' ' + bot.last_name;
@@ -162,13 +115,17 @@ var Game = function(id) {
 
 router.post('/', function (req, res, next) {
   var msg;
-  if (!req.body) {
+  if (!req.body.message) {
     msg = {
       id: '592503547',
-      from: 'Lau',
+      from: {
+        first_name: 'Bot Error'
+      },
       command: '/error',
       text: 'No clue what to do',
-      chat: 'test'
+      chat: {
+        id: '592503547'
+      }
     };
   } else {
     msg = {
@@ -179,8 +136,11 @@ router.post('/', function (req, res, next) {
       chat: req.body.message.chat
     };
   }
-  console.log(msg.id + ' - ' + req.body.message.from.first_name + ': ' + msg.command + ' -> ' + msg.text);
+  console.log(msg.id + ' - ' + msg.from.first_name + ': ' + msg.command + ' -> ' + msg.text);
   switch (msg.command) {
+    case '/error':
+      b.sendMessage(msg.chat.id, msg.text);
+      break;
     case '/start':
       b.sendMessage(msg.chat.id, 'To start a game, type /newgame');
       break;
@@ -201,7 +161,14 @@ router.post('/', function (req, res, next) {
       break;
     default:
       if (games[msg.chat.id]) {
-
+        games[msg.chat.id].list.values.forEach(function(item) {
+          if (item.value === msg.text.toLowerCase() && !item.guesser) {
+            item.guesser = msg.from;
+            b.sendMessage(msg.chat.id, msg.from.first_name + ' got ' + msg.text);
+          } else if (item.value === msg.text.toLowerCase() && !item.guesser) {
+            b.sendMessage(msg.chat.id, item.guesser.first_name + ' already guessed ' + msg.text + '/nToo bad, ' + msg.from.first_name);
+          }
+        });
       } else {
         b.sendMessage(msg.chat.id, 'Huh?');
       }
