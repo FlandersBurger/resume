75fbe5af33283371a32013213ffb571107725e0e
Laurent Debacker <debackerlaurent@gmail.com>
2018-11-01 09:05:14 -0700

    Fixed ampersand bug again, but better

--- bots/telegram.js
+++ bots/telegram.js
@@ -49,8 +49,9 @@ module.exports = function() {
 
 
   bot.sendMessage = function(channel, message) {
+    message = message.replace('&', '%26');
     return new Promise(function (resolve, reject) {
-      var url = 'https://api.telegram.org/bot' + bot.token + '/sendMessage?chat_id=' + channel + '&disable_notification=true&parse_mode=html&text=' + encodeURIComponent(message);
+      var url = 'https://api.telegram.org/bot' + bot.token + '/sendMessage?chat_id=' + channel + '&disable_notification=true&parse_mode=html&text=' + message;
       request(encodeURI(url), function (error, r, body) {
         var response = JSON.parse(body).result;
         //console.log(response);
