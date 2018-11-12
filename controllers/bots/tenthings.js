e95a184527bd81910d9e113237cdf2be4b46ba07
Laurent Debacker <debackerlaurent@gmail.com>
2018-11-11 17:41:24 -0800

    Bug that def needs work after this

--- controllers/bots/tenthings.js
+++ controllers/bots/tenthings.js
@@ -526,7 +526,7 @@ router.post('/', function (req, res, next) {
       return res.sendStatus(200);
     } else {
       //This group is spamming the bot
-      if (req.body.message.chat || req.body.edited_message.chat) {
+      if (req.body.message || req.body.edited_message) {
         if (req.body.message.chat.id === -1001376769922 || req.body.edited_message.chat.id === -1001376769922) {
           res.sendStatus(200);
         } else {
