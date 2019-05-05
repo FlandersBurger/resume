93a1b507ab0b3677430dbcf93fd3f87e8a2cd46b
Laurent Debacker <debackerlaurent@gmail.com>
2019-05-05 09:00:23 -0700

    Duplicated a line

--- controllers/bots/tenthings.js
+++ controllers/bots/tenthings.js
@@ -436,7 +436,6 @@ function guessed(game, msg, value, blurb, score, accuracy) {
   message += messages.streak(game.streak);
   message += blurb;
   message += '\n<pre>+' + score + ' points (' + accuracy + ')</pre>';
-  message += '\n<pre>+' + score + ' points (' + accuracy + ')</pre>';
   var answersLeft = game.list.values.filter(function(item) { return !item.guesser.first_name; }).length;
   if (answersLeft > 0) {
     message += '\n' + answersLeft + ' answer' + (answersLeft > 1 ? 's' : '') + ' left.';
