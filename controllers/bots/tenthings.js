f9803b9dd03c4cea602e5ed8be381474dc6d1d64
unknown <debackerlaurent@gmail.com>
2019-05-08 21:59:34 -0700

    Reduce for daily scores

--- controllers/bots/tenthings.js
+++ controllers/bots/tenthings.js
@@ -734,19 +734,14 @@ function getScores(gameId, scoreType) {
 }
 
 function getDailyScores(game, limit) {
-  var makeDailyScores = new Promise(function(resolve, reject) {
-    var str = game.players.filter(function(player) {
-      return player.scoreDaily;
-    }).sort(function(a, b) {
-      return b.scoreDaily - a.scoreDaily;
-    }).slice(0, limit ? limit : game.players.length).reduce(function(str, player, index) {
-      str += (index + 1) + ': ' + player.first_name + ' - ' + player.scoreDaily + '\n';
-    }, '<b>Daily Scores</b>\n');
-    resolve(str);
-  });
-  makeDailyScores.then(function(message) {
-    bot.sendMessage(game.chat_id, message);
-  });
+  var message = game.players.filter(function(player) {
+    return player.scoreDaily;
+  }).sort(function(a, b) {
+    return b.scoreDaily - a.scoreDaily;
+  }).slice(0, limit ? limit : game.players.length).reduce(function(str, player, index) {
+    str += (index + 1) + ': ' + player.first_name + ' - ' + player.scoreDaily + '\n';
+  }, '<b>Daily Scores</b>\n');
+  bot.sendMessage(game.chat_id, message);
 }
 
 function getList(game, callback) {
