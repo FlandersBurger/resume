62db95454b0d1e50bc5bd1175079a219db6101d5
Laurent Debacker <debackerlaurent@gmail.com>
2019-05-24 10:58:14 -0700

    removed result again

--- bots/telegram.js
+++ bots/telegram.js
@@ -143,7 +143,7 @@ b.init(TOKEN).then(function() {
   b.setWebhook('tenthings').then(function(body) {
        console.log(body);
     b.getWebhook().then(function(body) {
-       console.log(body.result);
+       console.log(body;
         b.introduceYourself();
     });
   });
