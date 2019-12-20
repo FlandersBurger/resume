43abbe470d8d98a900a0889c5230372fbbf41d5a
unknown <debackerlaurent@gmail.com>
2019-12-19 19:50:01 -0800

    Added hatch and start button to lemmings

--- ng/controllers/lemmings.ctrl.js
+++ ng/controllers/lemmings.ctrl.js
@@ -8,7 +8,10 @@ angular.module('app')
   var ctx = canvas.getContext('2d');
   var lemmingsImage = new Image();
   lemmingsImage.src = 'lemmings/lemmings.png';
+  var decorImage = new Image();
+  decorImage.src = 'lemmings/decor.png';
   var lemmings = {};
+  var started = false;
 
   var actions = {
     walk: {
@@ -121,6 +124,37 @@ angular.module('app')
     };
   }
 
+  function Hatch() {
+    this.position = [20, 20];
+    this.action = {
+      start: [0, 0],
+      end: [82, 500],
+      columns: 1,
+      rows: 10
+    }
+    this.width = (this.action.end[0] - this.action.start[0]) / this.action.columns;
+    this.height = (this.action.end[1] - this.action.start[1]) / this.action.rows;
+    this.cycle = 0;
+    this.open = function() {
+      ctx.save();
+      ctx.translate(this.position[0], this.position[1]);
+      ctx.translate(this.width / 2, this.height / 2);
+      var column, row;
+      if (started) {
+        column = this.cycle % this.action.columns;
+        row = Math.floor(this.cycle / this.action.columns);
+        if (this.cycle < 9) {
+          this.cycle++;
+        }
+      } else {
+        column = 0;
+        row = 0;
+      }
+      ctx.drawImage(decorImage, (this.width * column) + this.action.start[0], (this.height * row) + this.action.start[1] - 1, this.width, this.height, 0, 0, this.width * 2, this.height * 2);
+      ctx.restore();
+    }
+  }
+
   function spawnLemming() {
     Object.keys(actions).forEach(function(action) {
       lemmings[Math.round(Math.random() * 100000000)] = new Lemming(action);
@@ -145,13 +179,25 @@ angular.module('app')
 
 	initialize();
   spawnLemming();
+  let hatch = new Hatch()
   draw();
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i in lemmings) {
-      lemmings[i].move();
+    if (!started) {
+      ctx.beginPath();
+      ctx.font='40px Monoton';
+      ctx.lineWidth = '10';
+      ctx.strokeStyle = 'blue';
+      ctx.rect(15, canvas.height - 45, ctx.measureText('Start').width + 20, ctx.measureText('Start').height + canvas.height - 45);
+      ctx.fill();
+      ctx.fillText('Start', 20, canvas.height - 50);
+    } else {
+      for (var i in lemmings) {
+        lemmings[i].move();
+      }
     }
+    hatch.open();
     setTimeout(function() {
       draw();
     }, 150);
