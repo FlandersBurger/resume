d38d1c25f4d95037168f72e16247f9f3fa3073a6
unknown <debackerlaurent@gmail.com>
2020-02-26 22:29:17 -0800

    Add first snake logic

--- controllers/api/battlesnake.js
+++ controllers/api/battlesnake.js
@@ -12,12 +12,44 @@ router.post('/start', function ({ body }, res, next) {
     tailType: 'small-rattle'
   });
 });
-router.post('/move', function (req, res, next) {
+router.post('/move', function ({ body }, res, next) {
+  const me = body.you.body;
+
+  let board = new Array(body.board.width).fill(new Array(body.board.height).fill(0));
+  const food = body.board.food;
+  const snakes = body.board.snakes.reduce((positions, snake) => {
+    positions.concat(snake.body);
+  }, []);
+  food.forEach(item => {
+    board[item.x][item.y] = 1;
+  });
+  snakes.forEach(item => {
+    board[item.x][item.y] = -1;
+  });
+  let direction
+  if (me.x < body.board.width - 1 && board[me.x + 1][me.y] >= 0) {
+    direction = 'right';
+  } else if (me.x > 0 && board[me.x - 1][me.y] >= 0) {
+    direction = 'left';
+  } else if (me.y < body.board.width - 1 && board[me.x][me.y + 1] >= 0) {
+    direction = 'down';
+  } else if (me.y > 0 && board[me.x][me.y - 1] >= 0) {
+    direction = 'up';
+  }
+  res.json({
+    move: direction,
+    shout: 'Moving!'
+  });
 });
 router.post('/end', function (req, res, next) {
+  res.status(200).end();
 });
 router.post('/ping', function (req, res, next) {
   res.status(200).end();
 });
+
+const distance = (spot1, spot2) => {
+  return Math.abs(spot1.x - spot2.x) + Math.abs(spot1.y - spot2.y)
+};
 
 module.exports = router;
