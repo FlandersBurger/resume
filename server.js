4a1e4d017ae625760e9d46b0eeb3bd377c822991
unknown <debackerlaurent@gmail.com>
2020-02-26 22:36:16 -0800

    Open battlesnake api

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/api/email', require('./controllers/api/email'));
 app.use('/api/games', require('./controllers/api/games'));
 app.use('/api/bots', require('./controllers/api/bots'));
 app.use('/api/files', require('./controllers/api/files'));
+app.use('/api/battlesnake', require('./controllers/api/battlesnake'));
 
 app.use('/bots/tenthings', require('./controllers/bots/tenthings/main'));
 
