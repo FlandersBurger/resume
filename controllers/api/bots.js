003d9fdd4830e6d2432b95cfb6dea7b373db2548
unknown <debackerlaurent@gmail.com>
2020-04-03 22:33:53 -0700

    Limit select with lists

--- controllers/api/bots.js
+++ controllers/api/bots.js
@@ -20,62 +20,65 @@ var admins = [
   '5cd6d3a4597a396941793afe', //Maria
 ];
 
-router.get('/lists', function (req, res, next) {
-  List.find().populate('creator', 'username').exec(function(err, result) {
+router.get('/lists', (req, res, next) => {
+  List.find()
+  .select('_id plays skips score values date modifyDate creator name description category')
+  .populate('creator', 'username')
+  .exec((err, result) => {
     if (err) return next(err);
-    var lists = result.map(function(list) {
-      return {
+    var lists = result.map(list =>
+      ({
         _id: list._id,
         plays: list.plays,
         skips: list.skips,
         score: list.score,
         answers: list.values.length,
-        values: list.values.map(function(item) { return item.value; }),
-        blurbs: list.values.filter(function(item) { return item.blurb; }).length,
+        values: list.values.map(item => item.value),
+        blurbs: list.values.filter(item => item.blurb).length,
         date: list.date,
         modifyDate: list.modifyDate,
         creator: list.creator.username,
         name: list.name,
         description: list.description,
         category: list.category,
-      };
-    });
+      })
+    );
     res.json(lists);
   });
 });
 
-router.get('/lists/:id/report/:user', function (req, res, next) {
+router.get('/lists/:id/report/:user', (req, res, next) => {
   List.findOne({
     _id: req.params.id
   })
-  .exec(function(err, list) {
+  .exec((err, list) => {
     User.findOne({ _id: req.params.user })
-    .exec(function(err, user) {
+    .exec((err, user) => {
       bot.notifyAdmins('Check: ' + list.name + ' reported by ' + user.username);
     });
   });
 });
 
-router.get('/lists/:id', function (req, res, next) {
+router.get('/lists/:id', (req, res, next) => {
   List.findOne({
     _id: req.params.id
   })
   .populate('creator')
-  .exec(function(err, list) {
+  .exec((err, list) => {
     res.json(list);
   });
 });
 
-router.put('/lists', function (req, res, next) {
+router.put('/lists', (req, res, next) => {
   var yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
   var previousModifyDate = req.body.list.modifyDate;
   req.body.list.modifyDate = new Date();
   List.findByIdAndUpdate(req.body.list._id ? req.body.list._id : new mongoose.Types.ObjectId(), req.body.list, { new: true, upsert: true }, function(err, list) {
     if (err) return next(err);
-    List.findOne({
-      _id: list._id
-    }).populate('creator').exec(function(err, result) {
+    List.findOne({ _id: list._id })
+    .populate('creator')
+    .exec((err, result) => {
       if (err) return next(err);
       if (!req.body.list._id) {
         bot.notifyAdmins('<b>' + list.name + '</b> created by <i>' + req.body.user.username + '</i>');
@@ -87,16 +90,16 @@ router.put('/lists', function (req, res, next) {
   });
 });
 
-router.delete('/lists/:id', function (req, res, next) {
-  List.findByIdAndRemove(req.params.id, function(err, list) {
+router.delete('/lists/:id', (req, res, next) => {
+  List.findByIdAndRemove(req.params.id, (err, list) => {
     if (err) return next(err);
     bot.notifyAdmins(list.name + ' deleted');
     res.sendStatus(200);
   });
 });
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
   console.log(req.body);
-    res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 module.exports = router;
