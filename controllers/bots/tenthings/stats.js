a7bc879cd8c39888fe382dfcf757ce7062bb1bc0
unknown <debackerlaurent@gmail.com>
2020-05-07 07:35:41 -0700

    Stringify data

--- controllers/bots/tenthings/stats.js
+++ controllers/bots/tenthings/stats.js
@@ -116,7 +116,7 @@ exports.getList = (game, callback) => {
 
 exports.getStats = (chat_id, data, requestor) => {
   bot.sendMessage(chat_id, 'Temporarily disabled stats as someone is spamming the bot');
-  bot.notifyAdmin(data);
+  bot.notifyAdmin(JSON.stringify(data));
   const type = data.id.split('_')[0];
   const id = data.id.split('_')[1];
   let message = '';
