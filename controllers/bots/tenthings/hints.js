dc0e4ebdec6182abe13db23b6ec48bffe070caff
unknown <debackerlaurent@gmail.com>
2020-05-29 20:50:01 -0700

    Colon will reveal hints around it

--- controllers/bots/tenthings/hints.js
+++ controllers/bots/tenthings/hints.js
@@ -39,19 +39,19 @@ exports.getHint = (hints, value) => {
   for (i = 1; i < value.length - 2; i++) {
     switch (hints) {
       case 1:
-        if (i === 0 || /[ -().]/.test(value.charAt(i - 1))) {
+        if (i === 0 || /[ -():.]/.test(value.charAt(i - 1))) {
           str = str.substr(0, i) + value.charAt(i) + str.substr(i + 1);
         }
         break;
       default:
-        if (i === 0 || /[ -().]/.test(value.charAt(i - 1)) || /[ -().]/.test(value.charAt(i + 1)) || i === value.length - 1) {
+        if (i === 0 || /[ -():.]/.test(value.charAt(i - 1)) || /[ -():.]/.test(value.charAt(i + 1)) || i === value.length - 1) {
           str = str.substr(0, i) + value.charAt(i) + str.substr(i + 1);
         }
         break;
     }
   }
   return str;
-}
+};
 
 exports.getMaxHints = () => MAX_HINTS;
 exports.getSpecialCharacters = () => SPECIAL_CHARACTERS;
@@ -62,9 +62,12 @@ const conceal = (value, exceptions = '') =>
 function countLetters(string) {
   //Vowels get revealed all at once
   const alphabet = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
-  'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ];
   return alphabet.map(letter => ({
     letter,
-    count: (string.match(new RegExp(`[${letter}]`,'ig')) || []).length
-  })).filter(({count}) => count).sort((letter1, letter2) => letter1.count - letter2.count);
+    count: (string.match(new RegExp(`[${letter}]`, 'ig')) || []).length
+  })).filter(({
+    count
+  }) => count).sort((letter1, letter2) => letter1.count - letter2.count);
 }
