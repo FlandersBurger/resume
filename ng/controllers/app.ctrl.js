c7d2164362053a94e41cf4191a3d7a644927c0f8
unknown <debackerlaurent@gmail.com>
2020-06-20 21:02:07 -0700

    Added guess buttons

--- ng/controllers/app.ctrl.js
+++ ng/controllers/app.ctrl.js
@@ -1,180 +1,185 @@
 angular.module('app')
-.controller('AppCtrl', function ($scope, $location, LanguageSvc, UserSvc) {
+  .controller('AppCtrl', function($scope, $location, LanguageSvc, UserSvc) {
 
-  console.log("%cYou sneaky bugger!", "font: 2em sans-serif; color: DodgerBlue; text-shadow: 2px 0 0 #444, -2px 0 0 #444, 0 2px 0 #444, 0 -2px 0 #444, 1px 1px #444, -1px -1px 0 #444, 1px -1px 0 #444, -1px 1px 0 #444;");
-  console.log("I'm glad you're curious whether something is popping up in here. I'm a bit of a stickler when it comes to messages in the console so I try to make sure only things I want are visible. That being said, if there is a big error here, I would really appreciate you telling me so I can get rid of it!");
-  console.log("The code for my resume is hosted on Github if you really want to go into all this! => https://github.com/FlandersBurger/resume");
+    console.log("%cYou sneaky bugger!", "font: 2em sans-serif; color: DodgerBlue; text-shadow: 2px 0 0 #444, -2px 0 0 #444, 0 2px 0 #444, 0 -2px 0 #444, 1px 1px #444, -1px -1px 0 #444, 1px -1px 0 #444, -1px 1px 0 #444;");
+    console.log("I'm glad you're curious whether something is popping up in here. I'm a bit of a stickler when it comes to messages in the console so I try to make sure only things I want are visible. That being said, if there is a big error here, I would really appreciate you telling me so I can get rid of it!");
+    console.log("The code for my resume is hosted on Github if you really want to go into all this! => https://github.com/FlandersBurger/resume");
 
-  /*
-  $(document).bind("keyup keydown", function(e){
-    if(e.ctrlKey && e.keyCode == 80){
-        return false;
-    }
-  });
-  */
+    /*
+    $(document).bind("keyup keydown", function(e){
+      if(e.ctrlKey && e.keyCode == 80){
+          return false;
+      }
+    });
+    */
 
-  $(window).load(function(){
-    $(".loading").fadeOut("slow");
-    $(".content").fadeIn("slow");
-  });
+    $(window).load(function() {
+      $(".loading").fadeOut("slow");
+      $(".content").fadeIn("slow");
+    });
 
-  $scope.themeCounter = 6;
-  $scope.today = new Date();
-  $scope.year = $scope.today.getFullYear();
-  $scope.random = Math.floor((Math.random() * 1000000));
+    $scope.themeCounter = 6;
+    $scope.today = new Date();
+    $scope.year = $scope.today.getFullYear();
+    $scope.random = Math.floor((Math.random() * 1000000));
 
-  $scope.flipTheme = function () {
+    $scope.flipTheme = function() {
       $(".loading").show();
       $(".content").hide();
-      setTimeout(function(){
+      setTimeout(function() {
         $(".loading").fadeOut("slow");
         $(".content").fadeIn("slow");
       }, 800);
-    $scope.themeCounter = $scope.themeCounter < 6 ? $scope.themeCounter + 1 : $scope.themeCounter = 1;
-  };
+      $scope.themeCounter = $scope.themeCounter < 6 ? $scope.themeCounter + 1 : $scope.themeCounter = 1;
+    };
 
 
-  $.getJSON('/skills.json', function( data ) {
-    $scope.skills = data.filter(function(skill) {
-      return skill.enabled;
+    $.getJSON('/skills.json', function(data) {
+      $scope.skills = data.filter(function(skill) {
+        return skill.enabled;
+      });
     });
-  });
 
 
-  $.getJSON('/hobbies.json', function( data ) {
-    $scope.hobbies = data;
-  });
-
-
-  $.getJSON('/experience.json', function( data ) {
-    $scope.jobs = data;
-    $scope.jobs.forEach(function (job) {
-      job.startDate = new Date(job.startDate);
-      if (job.endDate) job.endDate = new Date(job.endDate);
+    $.getJSON('/hobbies.json', function(data) {
+      $scope.hobbies = data;
     });
-  });
 
-  $scope.getTimeSpan = function (job) {
-    return job.startDate.getFullYear() + (job.endDate ? (' - ' + job.endDate.getFullYear()) : ' - Today');
-  };
 
-  $scope.languages = LanguageSvc.languages;
+    $.getJSON('/experience.json', function(data) {
+      $scope.jobs = data;
+      $scope.jobs.forEach(function(job) {
+        job.startDate = new Date(job.startDate);
+        if (job.endDate) job.endDate = new Date(job.endDate);
+      });
+    });
 
-  $scope.selectedLanguage = LanguageSvc.getLanguage();
+    $scope.getTimeSpan = function(job) {
+      return job.startDate.getFullYear() + (job.endDate ? (' - ' + job.endDate.getFullYear()) : ' - Today');
+    };
 
-  $scope.selectLanguage = function (language) {
-    $scope.selectedLanguage = LanguageSvc.setLanguage(language);
-  };
+    $scope.languages = LanguageSvc.languages;
 
-  $scope.hoverdiv = function (e, divid) {
-    var left  = e.clientX + "px";
-    var top  = (e.clientY + 20) + "px";
+    $scope.selectedLanguage = LanguageSvc.getLanguage();
 
-    var div = document.getElementById(divid);
+    $scope.selectLanguage = function(language) {
+      $scope.selectedLanguage = LanguageSvc.setLanguage(language);
+    };
 
-    $("#"+divid).css('left',left);
-    $("#"+divid).css('top',top);
+    $scope.hoverdiv = function(e, divid) {
+      var left = e.clientX + "px";
+      var top = (e.clientY + 20) + "px";
 
-    $("#"+divid).toggle();
-    return false;
-  };
+      var div = document.getElementById(divid);
 
-  $scope.socialMedia = [/*
-    {
-      "name": "facebook",
-      "url": "https://www.facebook.com/flandersburger",
-      "icon": "fa-facebook-f"
-    },
-    {
-      "name": "twitter",
-      "url": "https://twitter.com/BelgoCanadian",
-      "icon": "fa-twitter"
-    },
-    {
-      "name": "goodreads",
-      "url": "https://www.goodreads.com/user/show/17070010-laurent",
-      "icon": "fa-book"
-    },*/
-    {
-      "name": "linkedin",
-      "url": "https://www.linkedin.com/in/laurent-debacker-1633a916",
-      "icon": "fa-linkedin"
-    },
-    {
-      "name": "github",
-      "url": "https://github.com/FlandersBurger",
-      "icon": "fa-github"
-    },
-    {
-      "name": "jsfiddle",
-      "url": "https://jsfiddle.net/user/BelgoCanadian/fiddles/",
-      "icon": "fa-jsfiddle"
-    },
-    {
-      "name": "stackoverflow",
-      "url": "http://stackoverflow.com/users/1083923/belgocanadian",
-      "icon": "fa-stack-overflow"
+      $("#" + divid).css('left', left);
+      $("#" + divid).css('top', top);
 
+      $("#" + divid).toggle();
+      return false;
+    };
+
+    $scope.socialMedia = [
+      /*
+          {
+            "name": "facebook",
+            "url": "https://www.facebook.com/flandersburger",
+            "icon": "fa-facebook-f"
+          },
+          {
+            "name": "twitter",
+            "url": "https://twitter.com/BelgoCanadian",
+            "icon": "fa-twitter"
+          },
+          {
+            "name": "goodreads",
+            "url": "https://www.goodreads.com/user/show/17070010-laurent",
+            "icon": "fa-book"
+          },*/
+      {
+        "name": "linkedin",
+        "url": "https://www.linkedin.com/in/laurent-debacker-1633a916",
+        "icon": "fa-linkedin"
+      },
+      {
+        "name": "github",
+        "url": "https://github.com/FlandersBurger",
+        "icon": "fa-github"
+      },
+      {
+        "name": "jsfiddle",
+        "url": "https://jsfiddle.net/user/BelgoCanadian/fiddles/",
+        "icon": "fa-jsfiddle"
+      },
+      {
+        "name": "stackoverflow",
+        "url": "http://stackoverflow.com/users/1083923/belgocanadian",
+        "icon": "fa-stack-overflow"
+
+      }
+    ];
+
+    if (window.localStorage.user) {
+      UserSvc.login(window.localStorage.user)
+        .then(function(response) {
+          login(response.data);
+        });
     }
-  ];
 
-  if (window.localStorage.user) {
-    UserSvc.login(window.localStorage.user)
-    .then(function(response) {
-      login(response.data);
+    $scope.$on('login', function(_, user) {
+      login(user);
     });
-  }
 
-  $scope.$on('login', function (_, user) {
-    login(user);
-  });
+    function login(user) {
+      if (!$scope.loggedIn) {
+        window.localStorage.user = user._id;
+        $scope.loggedIn = true;
+        $scope.currentUser = user;
+        $scope.$broadcast('login');
+      }
+    }
 
-  function login(user) {
-    if (!$scope.loggedIn) {
-      window.localStorage.user = user._id;
-      $scope.loggedIn = true;
+    $scope.$on('update', function(_, user) {
       $scope.currentUser = user;
-      $scope.$broadcast('login');
-    }
-  }
-
-  $scope.$on('update', function (_, user) {
-    $scope.currentUser = user;
-    $scope.currentUser.birthDate = new Date($scope.currentUser.birthDate);
-    $scope.loading = false;
-    setTimeout(function(){
-      $("#loading-icon").fadeOut("slow");
-    }, 1000);
-  });
-
-
-  $scope.$on('loading', function (_, user) {
-    $scope.loading = true;
-    $("#loading-icon").show();
-  });
-
-  $scope.$on('loaded', function (_, user) {
-    $scope.loading = false;
-    setTimeout(function(){
-      $("#loading-icon").fadeOut("slow");
-    }, 1000);
-  });
-
-  $scope.logout = function() {
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-      $scope.loggedIn = false;
-      window.localStorage.clear();
-      $scope.$broadcast('logout');
-    }).catch(function(error) {
-      // An error happened.
+      $scope.currentUser.birthDate = new Date($scope.currentUser.birthDate);
+      $scope.loading = false;
+      setTimeout(function() {
+        $("#loading-icon").fadeOut("slow");
+      }, 1000);
     });
-  };
 
-  $scope.formatDate = function(date) {
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    date = new Date(date);
-    return date.getDate() + "-" + months[date.getMonth()] + "-" + date.getFullYear();
-  };
 
-});
+    $scope.$on('loading', function(_, user) {
+      $scope.loading = true;
+      $("#loading-icon").show();
+    });
+
+    $scope.$on('loaded', function(_, user) {
+      $scope.loading = false;
+      setTimeout(function() {
+        $("#loading-icon").fadeOut("slow");
+      }, 1000);
+    });
+
+    $scope.logout = function() {
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        $scope.loggedIn = false;
+        window.localStorage.clear();
+        $scope.$broadcast('logout');
+      }).catch(function(error) {
+        // An error happened.
+      });
+    };
+
+    $scope.formatDate = function(date) {
+      var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      date = new Date(date);
+      return date.getDate() + "-" + months[date.getMonth()] + "-" + date.getFullYear();
+    };
+
+    String.prototype.capitalize = function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    };
+
+  });
--- ng/controllers/quizzes/animals.ctrl.js
+++ ng/controllers/quizzes/animals.ctrl.js
@@ -1,8 +1,26 @@
 angular.module('app')
   .controller('QuizAnimalsCtrl', function($scope, FileSvc) {
+
     FileSvc.getSounds('animals')
       .then(function(response) {
-        $scope.sounds = response.data;
-        console.log($scope.sounds);
+        $scope.animals = response.data.map(animal => ({
+          sound: animal,
+          name: animal.substring(0, animal.indexOf('.')).replace('_', ' ').capitalize()
+        }));
+        for (let animal of $scope.animals) {
+          animal.answers = [animal.name];
+          randomAnimals = getRandomAnimals(animal.name);
+          animal.answers.push(randomAnimals[0].name);
+          animal.answers.push(randomAnimals[1].name);
+        }
+        console.log($scope.animals);
       });
+
+    $scope.guess = (animal, guess) => {
+      animal.guess = guess;
+    };
+
+    const getRandomAnimals = name => _.shuffle($scope.animals.filter(animal => name !== animal.name));
+
+
   });
