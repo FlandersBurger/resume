15e2eb991b5af9e01b5a13e4744102f0569819e7
unknown <debackerlaurent@gmail.com>
2020-07-03 21:21:36 -0700

    Better queue details

--- controllers/bots/tenthings/main.js
+++ controllers/bots/tenthings/main.js
@@ -1497,7 +1497,7 @@ const getQueue = async () => {
   let message = `<b>Queue</b>\n`;
   message += `${count} correct answers queued\n`;
   message += `${outgoing} outgoing messages queued (max 30/sec)\n`;
-  message += `${webhook.pending_update_count} incoming messages pending in Telegram`;
+  message += `${webhook.pending_update_count} incoming messages pending in Telegram (max 100/sec)`;
   return message;
 };
 
