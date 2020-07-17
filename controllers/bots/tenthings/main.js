217e18358c671dc4837520f482af90f3c9503358
Laurent Debacker <debackerlaurent@gmail.com>
2020-07-17 07:36:00 -0700

    Remove 1 hour restriction

--- controllers/bots/tenthings/main.js
+++ controllers/bots/tenthings/main.js
@@ -839,7 +839,7 @@ router.post('/', async ({
     const date = body.message ? moment.unix(body.message.date) : moment();
     const message = body.message ? (body.message.text ? body.message.text : 'Not a callback or typed message') : body.callback_query.data;
     if (from != config.masterChat && await redis.get('pause') === 'true') return res.sendStatus(200);
-    if (date.diff(moment(), 'hours') > 1) return res.sendStatus(200);
+    //if (date.diff(moment(), 'hours') > 1) return res.sendStatus(200);
     if (BANNED_USERS.indexOf(from) >= 0) return res.sendStatus(200);
 
     if (antispam[from]) {
