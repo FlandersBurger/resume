3431d181e5d98f1e8f9636d7d7120777616641a7
unknown <debackerlaurent@gmail.com>
2020-08-15 09:58:42 -0700

    Are posts being received?

--- controllers/bots/tenthings/main.js
+++ controllers/bots/tenthings/main.js
@@ -829,6 +829,7 @@ const createMinigame = (game, msg) => new Promise(function(resolve, reject) {
 router.post('/', async ({
   body
 }, res, next) => {
+  console.log('here');
   if (body.object === 'page') {
     res.status(200).send('EVENT_RECEIVED');
     return console.log(body);
@@ -839,7 +840,6 @@ router.post('/', async ({
     const chat = body.message ? body.message.chat.id : body.callback_query.message.chat.id;
     const date = body.message ? moment.unix(body.message.date) : moment();
     const message = body.message ? (body.message.text ? body.message.text : 'Not a callback or typed message') : body.callback_query.data;
-    console.log(await redis.get('pause'));
     if (from != config.masterChat && await redis.get('pause') === 'true') return res.sendStatus(200);
     //if (date.diff(moment(), 'hours') > 1) return res.sendStatus(200);
     if (BANNED_USERS.indexOf(from) >= 0) return res.sendStatus(200);
