7a7ca0cf8e29b03cd4192533795e0996b1152110
Laurent Debacker <debackerlaurent@gmail.com>
2021-06-21 21:23:07 -0700

    again select with votes

--- controllers/bots/tenthings/lists.js
+++ controllers/bots/tenthings/lists.js
@@ -4,7 +4,6 @@ exports.getRandomList = async parameters => {
 	const count = await List.countDocuments(parameters).exec();
 	if (count === 0) return;
 	const lists = await List.find(parameters)
-		.select('-votes')
 		.populate('creator')
 		.limit(1)
 		.skip(Math.floor(Math.random() * count))
