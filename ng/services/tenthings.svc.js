c2a54f1459bcbc08ab620d598268a6676f2c499b
Laurent Debacker <debackerlaurent@gmail.com>
2023-05-25 13:37:29 -0700

    proper pagination

--- ng/services/tenthings.svc.js
+++ ng/services/tenthings.svc.js
@@ -1,78 +1,78 @@
 angular.module('app').service('TenThingsSvc', function ($http) {
-	var svc = this;
+  var svc = this;
 
-	svc.getQueue = function () {
-		return $http.get(`/bots/tenthings/queue`);
-	};
+  svc.getQueue = function () {
+    return $http.get(`/bots/tenthings/queue`);
+  };
 
-	svc.getLists = function () {
-		return $http.get(`/api/tenthings/lists`);
-	};
+  svc.getLists = function () {
+    return $http.get(`/api/tenthings/lists`);
+  };
 
-	svc.getCategories = () => {
-		return $http.get(`/api/tenthings/categories`);
-	};
+  svc.getCategories = () => {
+    return $http.get(`/api/tenthings/categories`);
+  };
 
-	svc.getLanguages = () => {
-		return $http.get(`/api/tenthings/languages`);
-	};
+  svc.getLanguages = () => {
+    return $http.get(`/api/tenthings/languages`);
+  };
 
-	svc.getList = function (list) {
-		return $http.get('/api/tenthings/lists/' + list._id);
-	};
+  svc.getList = function (list) {
+    return $http.get('/api/tenthings/lists/' + list._id);
+  };
 
-	svc.updateList = function (list) {
-		return $http.post('/api/tenthings/lists/' + list._id, list);
-	};
+  svc.updateList = function (list) {
+    return $http.post('/api/tenthings/lists/' + list._id, list);
+  };
 
-	svc.saveList = function (user, list) {
-		return $http.put('/api/tenthings/lists', {
-			user: user,
-			list: list,
-		});
-	};
+  svc.saveList = function (user, list) {
+    return $http.put('/api/tenthings/lists', {
+      user: user,
+      list: list,
+    });
+  };
 
-	svc.reportList = function (user, list) {
-		$http.get('/api/tenthings/lists/' + list._id + '/report/' + user._id);
-	};
+  svc.reportList = function (user, list) {
+    $http.get('/api/tenthings/lists/' + list._id + '/report/' + user._id);
+  };
 
-	svc.deleteList = function (list) {
-		return $http.delete('/api/tenthings/lists/' + list._id);
-	};
+  svc.deleteList = function (list) {
+    return $http.delete('/api/tenthings/lists/' + list._id);
+  };
 
-	svc.getMoviePics = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/movies`);
-	};
+  svc.getMoviePics = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/movies`);
+  };
 
-	svc.getTVPics = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/tv`);
-	};
+  svc.getTVPics = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/tv`);
+  };
 
-	svc.getActorPics = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/actors`);
-	};
+  svc.getActorPics = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/actors`);
+  };
 
-	svc.getBookPics = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/books`);
-	};
+  svc.getBookPics = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/books`);
+  };
 
-	svc.getMusicVideos = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/musicvideos`);
-	};
+  svc.getMusicVideos = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/musicvideos`);
+  };
 
-	svc.getPics = function (list) {
-		return $http.get(`/api/tenthings/lists/${list._id}/pics`);
-	};
+  svc.getPics = function (list) {
+    return $http.get(`/api/tenthings/lists/${list._id}/pics`);
+  };
 
-	svc.getPause = function () {
-		return $http.get(`/api/tenthings/pause`);
-	};
+  svc.getPause = function () {
+    return $http.get(`/api/tenthings/pause`);
+  };
 
-	svc.togglePause = function () {
-		return $http.post(`/api/tenthings/pause`);
-	};
+  svc.togglePause = function () {
+    return $http.post(`/api/tenthings/pause`);
+  };
 
-	svc.getGame = function (id) {
-		return $http.get(`/api/tenthings/game/${id}`);
-	};
+  svc.getGame = function (id) {
+    return $http.get(`/api/tenthings/game/${id}`);
+  };
 });
