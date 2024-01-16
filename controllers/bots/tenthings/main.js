f31454d53b77f5de43b821124bc88d9f2d3c4f6a
Laurent Debacker <debackerlaurent@gmail.com>
2024-01-15 23:15:28 -0800

    change commands check

--- controllers/bots/tenthings/main.js
+++ controllers/bots/tenthings/main.js
@@ -346,6 +346,7 @@ router.post("/", async ({ body, get }, res, next) => {
           game.save(async (err, savedGame) => {
             if (err) return bot.notifyAdmin(`Language Save Error: \n${JSON.stringify(err)}`);
             bot.answerCallback(body.callback_query.id, `${data.id} -> New bot language`);
+            bot.setCommands(data.chat_id, data.id);
             const availableLanguages = await List.aggregate([
               { $group: { _id: "$language", count: { $sum: 1 } } },
             ]).exec();
