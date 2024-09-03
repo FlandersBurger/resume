f4de7317ba88fa68ab83b994baf81686fc357e23
Laurent Debacker <debackerlaurent@gmail.com>
2024-09-02 21:53:14 -0700

    remove topic

--- src/connections/telegram.ts
+++ src/connections/telegram.ts
@@ -163,13 +163,10 @@ class TelegramBot {
     message: string,
     options: { topic?: number; replyMessageId?: string; replyMarkup?: ReplyMarkup } = {},
   ) => {
-    const { topic, replyMessageId, replyMarkup } = options;
+    const { replyMessageId, replyMarkup } = options;
     message = encodeURIComponent(message);
     let url = `${this.baseUrl}/sendMessage?chat_id=${channel}&disable_notification=true&parse_mode=html&text=${message}`;
-    if (topic) {
-      url += `&message_thread_id=${topic}`;
-      console.log(topic);
-    }
+    // if (topic) url += `&message_thread_id=${topic}`;
     if (replyMessageId) {
       url += `&reply_markup=${JSON.stringify({ force_reply: true, selective: true })}`;
       url += `&reply_parameters=${JSON.stringify({ message_id: replyMessageId, allow_sending_without_reply: true })}`;
